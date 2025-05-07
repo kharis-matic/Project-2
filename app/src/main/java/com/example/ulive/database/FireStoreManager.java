12028441a68f9e901212133cf709faa19d403378
endino-michaelarjkate <endino.michaelarjkate@gmail.com>
2025-05-07 23:42:31 +0800

    initial

---
+++ app/src/main/java/com/example/ulive/database/FireStoreManager.java
@@ -0,0 +1,11 @@
+package com.example.ulive.database;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FireStoreManager {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
+}
--- app/src/main/java/com/example/ulive/models/User.java
+++ app/src/main/java/com/example/ulive/models/User.java
@@ -1,4 +1,94 @@
 package com.example.ulive.models;
 
+import com.google.firebase.firestore.Exclude;
+
 public class User {
+    private String id;
+    private String type; // "Renter" or "Owner"
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender; // "Female" or "Male"
+    private boolean activeStatus;
+    private String phoneNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isActiveStatus() {
+        return activeStatus;
+    }
+
+    public void setActiveStatus(boolean activeStatus) {
+        this.activeStatus = activeStatus;
+    }
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String gender, String type, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.type = type;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.activeStatus = true;
+    }
+
+    @Exclude
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 }
+
+
