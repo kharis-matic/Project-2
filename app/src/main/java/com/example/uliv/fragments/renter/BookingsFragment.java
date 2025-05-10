404b372ab67e01e42e0fbab5683ee8d47c4b67ba
Ken <kenftabianan@gmail.com>
2025-05-10 22:23:20 +0800

    Bookings and Enquiries UI

--- app/src/main/java/com/example/uliv/fragments/renter/BookingsFragment.java
+++ app/src/main/java/com/example/uliv/fragments/renter/BookingsFragment.java
@@ -17,7 +17,6 @@ public class BookingsFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
