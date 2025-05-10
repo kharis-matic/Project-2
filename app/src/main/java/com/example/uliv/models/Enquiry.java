79cf7b966472151f4fb126040cae9a600e7aa56c
endino-michaelarjkate <endino.michaelarjkate@gmail.com>
2025-05-10 13:57:36 +0800

    models

---
+++ app/src/main/java/com/example/uliv/models/Enquiry.java
@@ -0,0 +1,47 @@
+package com.example.uliv.models;
+
+public class Enquiry {
+    private String id;
+    private String renterId;
+    private String roomId;
+    private String status; // "PENDING" or "ACCEPTED" or "REJECTED"
+
+    public Enquiry(String id, String renterId, String roomId, String status) {
+        this.id = id;
+        this.renterId = renterId;
+        this.roomId = roomId;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRenterId() {
+        return renterId;
+    }
+
+    public void setRenterId(String renterId) {
+        this.renterId = renterId;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ app/src/main/java/com/example/uliv/models/Room.java
@@ -0,0 +1,67 @@
+package com.example.uliv.models;
+
+public class Room {
+    private String id;
+    private String propertyId;
+    private boolean availabilityStatus;
+    private int bedspaceCount;
+    private double monthlyRentalFee;
+    private String description;
+
+    public Room(String id, String propertyId, boolean availabilityStatus, int bedspaceCount, double monthlyRentalFee, String description) {
+        this.id = id;
+        this.propertyId = propertyId;
+        this.availabilityStatus = availabilityStatus;
+        this.bedspaceCount = bedspaceCount;
+        this.monthlyRentalFee = monthlyRentalFee;
+        this.description = description;
+    }
+
+    public boolean isAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public void setAvailabilityStatus(boolean availabilityStatus) {
+        this.availabilityStatus = availabilityStatus;
+    }
+
+    public int getBedspaceCount() {
+        return bedspaceCount;
+    }
+
+    public void setBedspaceCount(int bedspaceCount) {
+        this.bedspaceCount = bedspaceCount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getMonthlyRentalFee() {
+        return monthlyRentalFee;
+    }
+
+    public void setMonthlyRentalFee(double monthlyRentalFee) {
+        this.monthlyRentalFee = monthlyRentalFee;
+    }
+
+    public String getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(String propertyId) {
+        this.propertyId = propertyId;
+    }
+}
